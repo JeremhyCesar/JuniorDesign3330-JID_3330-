eacac20c02fbdb7092b8241f11f2477ba115977b
Benjamin <benjabee@comcast.net>
2024-02-09 12:47:18 -0500

    Home

---
+++ src/pages/Home.js
@@ -0,0 +1,111 @@
+import { Pressable, StyleSheet, ScrollView, Text, View, Image } from 'react-native';
+
+export function Home({ navigation }) {
+    const styles = StyleSheet.create({
+        button: {
+            borderRadius: 31,
+            shadowOffset: {width: 4, height: 4},
+            shadowColor: 'black',
+            shadowOpacity: 0.25,
+            width: '84%',
+            height: 250,
+            cursor: 'pointer'
+        }
+    })
+    return (
+        <ScrollView style={{backgroundColor: 'white'}}>
+            <Text style={{
+                fontWeight: 'bold',
+                top: 32,
+                left: '8%',
+                fontSize: 48,
+                color: '#e2480d'
+            }}>Julie Smith</Text>
+            <Text style={{
+                top: 36,
+                left: '8%',
+                fontSize: 20,
+                textAlign: 'left',
+                color: '#717171'
+            }}>Happy Music Monday!</Text>
+            <View style={{
+                top: 60,
+                left: '8%',
+                borderRadius: 31,
+                backgroundColor: 'white',
+                shadowOffset: {width: 4, height: 4},
+                shadowColor: 'black',
+                shadowOpacity: 0.25,
+                width: '84%',
+                height: 142
+            }}>
+                <Text style={{
+                    top: '25%',
+                    left: '15%',
+                    fontSize: 30,
+                    fontWeight: 500,
+                    width: '70%',
+                    shadowOffset: {width: 4, height: 4},
+                    shadowColor: 'black',
+                    shadowOpacity: 0.25,
+                    color: '#717171',
+                    flex: 1,
+                    flexWrap: 'wrap',
+                    textAlign: 'center'
+                }}>Where words fail, music speaks.</Text>
+            </View>
+            <Pressable onPress={() => {navigation.navigate('Lessons')}} style={[{top: 94, left: '8%', backgroundColor: '#ffbb37'}, styles.button]}>
+                <Text style={{
+                    top: 26,
+                    left: '10%',
+                    fontSize: 48,
+                    textAlign: 'left',
+                    fontWeight: 'bold',
+                    color: 'white'
+                }}>Learn</Text>
+                <Image source={require('../../assets/teaching-icon.png')} style={{
+                    top: 10,
+                    left: '45%',
+                    width: '45%',
+                    height: 186,
+                    objectFit: 'cover'
+                }}/>
+            </Pressable>
+            <View style={[{top: 126, left: '8%', backgroundColor: '#e24808'}, styles.button]}>
+                <Text style={{
+                    top: 26,
+                    left: '10%',
+                    fontSize: 48,
+                    textAlign: 'left',
+                    fontWeight: 'bold',
+                    color: 'white'
+                }}>Listen</Text>
+                <Image source={require('../../assets/girl-enjoying-music.png')} style={{
+                    top: 10,
+                    left: '43%',
+                    width: '50%',
+                    height: 160,
+                    objectFit: 'cover'
+                }}/>
+            </View>
+            <View style={[{top: 160, left: '8%', backgroundColor: '#00347f'}, styles.button]}>
+                <Text style={{
+                    top: 26,
+                    left: '10%',
+                    fontSize: 48,
+                    textAlign: 'left',
+                    fontWeight: 'bold',
+                    color: 'white'
+                }}>Quizzes</Text>
+                <Image source={require('../../assets/to-do-list.png')} style={{
+                    top: 30,
+                    left: '55%',
+                    width: '35%',
+                    height: 120,
+                    objectFit: 'cover'
+                }}/>
+            </View>
+            <View style={{height: 180}}/>
+        </ScrollView>
+    );
+}
