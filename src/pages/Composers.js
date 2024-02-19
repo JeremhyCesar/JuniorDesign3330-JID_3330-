0160efe28edd15fe07cf11324611cdfccb00e8bd
helloalpacaa <helloalpacaa@gmail.com>
2024-02-19 09:25:09 -0500

    Add Chopin

--- src/pages/Composers.js
+++ src/pages/Composers.js
@@ -1,6 +1,6 @@
-import { StyleSheet, ScrollView, Text, View, Image, Pressable } from 'react-native';
+import { StyleSheet, ScrollView, Text, View, Image } from 'react-native';
 
-export function ClassicalComposers({navigation}) {
+export function Composers() {
     const styles = StyleSheet.create({
         button: {
             borderRadius: 30,
@@ -16,23 +16,23 @@ export function ClassicalComposers({navigation}) {
         <ScrollView style={{backgroundColor: 'white'}}>
             <Text style={{
                 fontWeight: 'bold',
-                top: 64,
+                top: 32,
                 left: '8%',
                 fontSize: 48,
                 color: '#333'
             }}>Composers</Text>
             <Text style={{
-                top: 68,
+                top: 36,
                 left: '8%',
                 fontSize: 20,
                 textAlign: 'left',
                 color: '#717171'
             }}>The minds behind the music!</Text>
-            <Pressable onPress={() => navigation.navigate('Chopin')} style={[{top: 94, left: '8%', backgroundColor: '#ffffff'}, styles.button]}>
+            <View style={[{top: 94, left: '8%', backgroundColor: '#ffffff'}, styles.button]}>
                 <Text style={{
                     top: 26,
-                    left: '8%',
-                    fontSize: 30,
+                    left: '10%',
+                    fontSize: 32,
                     textAlign: 'left',
                     fontWeight: 'bold',
                     color: '#dc7424',
@@ -41,51 +41,51 @@ export function ClassicalComposers({navigation}) {
                 }}>Frédéric Chopin</Text>
                 <Image source={require('../../assets/chopin.png')} style={{
                     position: 'absolute',
-                    top: 5,
+                    top: 10,
                     left: '55%',
                     width: 120,
-                    height: 150,
+                    height: 120,
                     objectFit: 'cover'
                 }}/>
-            </Pressable>
+            </View>
             <View style={[{top: 109, left: '8%', backgroundColor: '#ffffff'}, styles.button]}>
                 <Text style={{
-                    top: 25,
-                    left: '8%',
-                    fontSize: 30,
+                    top: 11,
+                    left: '10%',
+                    fontSize: 32,
                     textAlign: 'left',
                     fontWeight: 'bold',
                     color: '#dc7424',
-                    width: 165,
+                    width: 150,
                     flexWrap: 'wrap'
                 }}>Ludwig Van Beethoven</Text>
                 <Image source={require('../../assets/Beethoven.png')} style={{
                     position: 'absolute',
-                    top: 0,
+                    top: 10,
                     left: '55%',
                     width: 120,
-                    height: 155,
+                    height: 120,
                     objectFit: 'cover'
                 }}/>
             </View>
             
             <View style={[{top: 124, left: '8%', backgroundColor: '#ffffff'}, styles.button]}>
                 <Text style={{
-                    top: 40,
-                    left: '8%',
-                    fontSize: 30,
+                    top: 26,
+                    left: '10%',
+                    fontSize: 32,
                     textAlign: 'left',
                     fontWeight: 'bold',
                     color: '#dc7424',
                     width: 150,
                     flexWrap: 'wrap'
-                }}>J. S. Bach</Text>
+                }}>Johannes Bach</Text>
                 <Image source={require('../../assets/Bach.png')} style={{
                     position: 'absolute',
-                    top: 0,
+                    top: 10,
                     left: '55%',
                     width: 120,
-                    height: 150,
+                    height: 120,
                     objectFit: 'cover'
                 }}/>
             </View>
