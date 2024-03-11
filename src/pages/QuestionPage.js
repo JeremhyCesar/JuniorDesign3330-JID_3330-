61f53f8c61175087d0e0b7445b1f9b8f19863b95
Jeremhy Cesar <jcesar7@gatech.edu>
2024-03-10 23:16:32 -0400

    Created sample infrastructure for question bank for quiz questions

---
+++ src/pages/QuestionPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Pressable, StyleSheet, ScrollView, Text, View, Image } from 'react-native';
+
+var questionBank = new XMLHttpRequest();
+/**
+qu.open('GET', '/foo.txt');
+client.onreadystatechange = function() {
+  alert(client.responseText);
+}
+*/
