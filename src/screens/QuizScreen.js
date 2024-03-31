8ed2bba3fe32904852fc2e8b89c2caa619b3f5bd
helloalpacaa <helloalpacaa@gmail.com>
2024-03-31 09:33:08 -0400

    Update QuizScreen

--- src/screens/QuizScreen.js
+++ src/screens/QuizScreen.js
@@ -12,7 +12,7 @@ import {
 
 export function QuizScreen({ navigation }) {
   const styles = StyleSheet.create({
-    button: {
+    block: {
       borderRadius: 31,
       shadowOffset: { width: 4, height: 4 },
       shadowColor: "black",
@@ -20,8 +20,35 @@ export function QuizScreen({ navigation }) {
       width: "84%",
       height: 250,
       cursor: "pointer",
+      paddingHorizontal: 20,
+      paddingVertical: 15,
+    },
+    title: {
+      fontSize: 28,
+      textAlign: "left",
+      fontWeight: "bold",
+      color: "white",
+      marginBottom: 15,
+    },
+    buttonContainer: {
+      flexDirection: "column",
+      justifyContent: "flex-start",
+      alignItems: "flex-start",
+    },
+    button: {
+      backgroundColor: "white",
+      borderRadius: 20,
+      paddingHorizontal: 15,
+      paddingVertical: 10,
+      marginBottom: 10,
+    },
+    buttonText: {
+      fontSize: 18,
+      textAlign: "center",
+      fontWeight: "bold",
     },
   });
+
   return (
     <ScrollView style={{ backgroundColor: "white" }}>
       <Text
@@ -46,174 +73,103 @@ export function QuizScreen({ navigation }) {
       >
         Test your knowledge
       </Text>
-      <Pressable
-        onPress={() => navigation.navigate("QuizTopic")}
+      <View
         style={[
           { top: 94, left: "8%", backgroundColor: "#ffbb37" },
-          styles.button,
+          styles.block,
         ]}
       >
-        <Text
-          style={{
-            top: 26,
-            left: "10%",
-            fontSize: 28,
-            textAlign: "left",
-            fontWeight: "bold",
-            color: "white",
-            width: 200,
-            flexWrap: "wrap",
-          }}
-        >
-          Online Quiz
-        </Text>
+        <Text style={styles.title}>Online Quiz</Text>
+        <View style={styles.buttonContainer}>
+          <Pressable
+            style={[styles.button, { marginBottom: 20 }]}
+            onPress={() => navigation.navigate("CreateOnlineQuiz")}
+          >
+            <Text style={[styles.buttonText, { color: "#ffbb37" }]}>
+              Create Room
+            </Text>
+          </Pressable>
+          <Pressable
+            style={styles.button}
+            onPress={() => navigation.navigate("JoinOnlineQuiz")}
+          >
+            <Text style={[styles.buttonText, { color: "#ffbb37" }]}>
+              Join Room
+            </Text>
+          </Pressable>
+        </View>
         <Image
           source={require("../../assets/girl-playing-violin.png")}
           style={{
             position: "absolute",
             top: 0,
-            left: "45%",
+            right: 30,
             width: 160,
             height: 250,
             objectFit: "cover",
           }}
         />
-        {/* <View
-          style={{
-            borderRadius: 31,
-            backgroundColor: "white",
-            width: 200,
-            height: 9,
-            marginLeft: 83,
-            top: 184,
-          }}
-        />
-        <Text
-          style={{
-            top: 186,
-            marginLeft: 135,
-            fontSize: 13,
-            textAlign: "left",
-            fontWeight: "bold",
-            color: "white",
-            flexWrap: "wrap",
-          }}
-        >
-          0% completed
-        </Text> */}
-      </Pressable>
-      <Pressable
-        onPress={() => navigation.navigate("QuizTopic")}
+      </View>
+      <View
         style={[
           { top: 124, left: "8%", backgroundColor: "#e24808" },
-          styles.button,
+          styles.block,
         ]}
       >
-        <Text
-          style={{
-            top: 26,
-            left: "10%",
-            fontSize: 28,
-            textAlign: "left",
-            fontWeight: "bold",
-            color: "white",
-            width: "55%",
-            flexWrap: "wrap",
-          }}
-        >
-          Offline Quiz
-        </Text>
+        <Text style={styles.title}>Offline Quiz</Text>
+        <View style={styles.buttonContainer}>
+          <Pressable
+            style={[styles.button, { marginBottom: 20 }]}
+            onPress={() => navigation.navigate("QuizTopic")}
+            // Change this onPress to navigate to the OfflineQuiz screen once it is created
+            // onPress={() => navigation.navigate("OfflineQuiz")}
+          >
+            <Text style={[styles.buttonText, { color: "#e24808" }]}>
+              Start Practicing
+            </Text>
+          </Pressable>
+        </View>
         <Image
           source={require("../../assets/couple-dance-party.png")}
           style={{
             position: "absolute",
             top: 45,
-            left: "35%",
+            right: 0,
             width: 220,
             height: 180,
             objectFit: "cover",
           }}
         />
-        {/* <View
-          style={{
-            borderRadius: 31,
-            backgroundColor: "white",
-            width: 200,
-            height: 9,
-            marginLeft: 83,
-            top: 184,
-          }}
-        />
-        <Text
-          style={{
-            top: 186,
-            marginLeft: 135,
-            fontSize: 13,
-            textAlign: "left",
-            fontWeight: "bold",
-            color: "white",
-            flexWrap: "wrap",
-          }}
-        >
-          0% completed
-        </Text> */}
-      </Pressable>
-      <Pressable
-        onPress={() => navigation.navigate("QuizTopic")}
+      </View>
+      <View
         style={[
           { top: 154, left: "8%", backgroundColor: "#00347f" },
-          styles.button,
+          styles.block,
         ]}
       >
-        <Text
-          style={{
-            top: 26,
-            left: "10%",
-            fontSize: 28,
-            textAlign: "left",
-            fontWeight: "bold",
-            color: "white",
-            width: "50%",
-            flexWrap: "wrap",
-          }}
-        >
-          Create Quiz
-        </Text>
+        <Text style={styles.title}>Create Quiz</Text>
+        <View style={styles.buttonContainer}>
+          <Pressable
+            style={[styles.button, { marginBottom: 20 }]}
+            onPress={() => navigation.navigate("CreateQuiz")}
+          >
+            <Text style={[styles.buttonText, { color: "#00347f" }]}>
+              Create New Quiz
+            </Text>
+          </Pressable>
+        </View>
         <Image
           source={require("../../assets/saxophonist.png")}
           style={{
             position: "absolute",
             top: 30,
-            left: "50%",
+            right: 10,
             width: 160,
             height: 200,
             objectFit: "cover",
           }}
         />
-        {/* <View
-          style={{
-            borderRadius: 31,
-            backgroundColor: "white",
-            width: 200,
-            height: 9,
-            marginLeft: 83,
-            top: 184,
-          }}
-        />
-        <Text
-          style={{
-            top: 186,
-            marginLeft: 135,
-            fontSize: 13,
-            textAlign: "left",
-            fontWeight: "bold",
-            color: "white",
-            flexWrap: "wrap",
-          }}
-        >
-          0% completed
-        </Text> */}
-      </Pressable>
+      </View>
       <View style={{ height: 180 }} />
     </ScrollView>
   );
